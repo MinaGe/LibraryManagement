da3b291f592c92c6405e6479b81b3a26fb3d3856
mina gerges <minagerges651@gmail.com>
2020-10-05 01:41:16 +0200

    return

---
+++ LibraryManagement/Controllers/Return.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class Return : Controller
+    {
+        private readonly IBookRepositery bookRepositery;
+        private readonly ICuntomerRepositery cuntomerRepositery;
+
+        public Return(IBookRepositery bookRepositery, ICuntomerRepositery cuntomerRepositery)
+        {
+            this.bookRepositery = bookRepositery;
+            this.cuntomerRepositery = cuntomerRepositery;
+        }
+
+
+        [Route("Return")]
+        public IActionResult List()
+        {
+            // load all borrowed books
+            var borrowedBooks = bookRepositery.FindWithAuthorAndBorrower(x => x.CustomerId != 0);
+            // Check the books collection
+            if (borrowedBooks == null || borrowedBooks.ToList().Count() == 0)
+            {
+                return View("Empty");
+            }
+            return View(borrowedBooks);
+        }
+
+
+        public IActionResult ReturnABook(int bookId)
+        {
+            // load the current book
+            var book = bookRepositery.GetById(bookId);
+            // remove borrower
+            book.Customer = null;
+
+            book.CustomerId = 0;
+            // update database
+            bookRepositery.Update(book);
+            // redirect to list method
+            return RedirectToAction("List");
+        }
+    }
+}
